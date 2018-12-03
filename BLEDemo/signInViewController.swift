8318f067ab43d9e022a2e7bdc3e50ecdc9b824e5
Abdulaziz Alharbi <abdulazizalharbi@AbdulazzsAir133.T-mobile.com>
2018-12-02 18:14:21 -0800

    adding constraints, seguaes, before Firebase, and bluetooth is working

---
+++ BLEDemo/signInViewController.swift
@@ -0,0 +1,41 @@
+//
+//  signInViewController.swift
+//  BLEDemo
+//
+//  Created by Abdulaziz Alharbi on 12/2/18.
+//  Copyright © 2018 Rick Smith. All rights reserved.
+//
+
+import UIKit
+
+class signInViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signInPressed(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BLEDemo/signUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  signUpViewController.swift
+//  BLEDemo
+//
+//  Created by Abdulaziz Alharbi on 12/2/18.
+//  Copyright © 2018 Rick Smith. All rights reserved.
+//
+
+import UIKit
+
+class signUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
